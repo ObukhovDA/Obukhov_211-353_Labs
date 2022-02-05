a6de302a741869e84944b223311906ce94a69d47
Twist <90751048+EgorSHav@users.noreply.github.com>
2022-02-05 09:37:13 +0300

    Add files via upload

---
+++ 6.1/6.1/6.1.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <queue>
+
+using namespace std;
+int Check(int a)
+{
+    while (a % 2 == 0)
+    {
+        a /= 2;
+    }
+    while (a % 3 == 0)
+    {
+        a /= 3;
+    }
+    while (a % 5 == 0)
+    {
+        a /= 5;
+    }
+
+}
+
+int main()
+{
+    queue<int> que;
+    int n;
+    int num = 2;
+    int count = 0;
+
+    cout << "N: " << endl;
+    cin >> n;
+    while (count < n)
+    {
+        if (Check(num) == 1)
+        {
+            que.push(num);
+            count++;
+        }
+        num++;
+    }
+}
