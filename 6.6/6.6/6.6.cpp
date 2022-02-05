8165a0ee2ace01cbb1d102ab11d16ed1419c0d00
Twist <90751048+EgorSHav@users.noreply.github.com>
2022-02-05 09:43:02 +0300

    Add files via upload

---
+++ 6.6/6.6/6.6.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <string>
+#include <map>
+using namespace std;
+int main() 
+{
+    string input;
+    map<string, int> mp;
+
+    double count = 0;
+    while (cin >> input)
+    {
+        if (input == "1") 
+        {
+            break;
+        }
+        count++;
+        if (mp.find(input) == mp.end()) 
+        {
+            mp[input] = 1;
+        }
+        else
+        {
+            mp[input] += 1;
+        }
+    }
+
+    for (auto i : mp) 
+    {
+        cout << i.first << " " << (double(i.second / count) * 100) << "%" << endl;
+    }
+}
