f96f52993ae95afc94d5e2c3c7a9ef9b7b79d7df
Twist <90751048+EgorSHav@users.noreply.github.com>
2022-02-05 09:38:18 +0300

    Add files via upload

---
+++ 6.4/6.4/6.4.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <string>
+#include <set>
+using namespace std;
+int main()
+{
+    set<string> st;
+    string input = "";
+    while (true)
+    {
+        cout << "Login: " << endl;
+        cin >> input;
+        if (st.find(input) == st.end())
+        {
+            cout << "Login True: " << input << endl << endl;
+            st.insert(input);
+        }
+        else 
+        {
+            cout << "Login False: " << input << endl << endl;
+        }
+    }
+}
