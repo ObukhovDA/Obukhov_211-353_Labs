08cff89d54a2434ffd4e4949d3c4da30b7fd355c
Twist <90751048+EgorSHav@users.noreply.github.com>
2022-02-05 09:33:50 +0300

    lab5.4

---
+++ 5.4/5.4.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include "Position.h"
+#include "Circle.h"
+#include "Square.h"
+#include "CircleSquare.h"
+
+int main() {
+	CircleSquare cis(3, 2, 6);
+	
+	cis.print();
+	cout << endl;
+	cis.Circle::printData();
+	cout << endl;
+	cis.Square::printData();
+	cout << endl;
+	cis.changeCoordinate(3, 4);
+	cis.print();
+}
